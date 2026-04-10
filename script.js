b8c411b0293f4091b32d183971db341eac9ba245
SrijanG05 <gsrijan777@gmail.com>
2026-04-10 14:32:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function makeid(l) {
   // write your code here
+	 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  
+  for (let i = 0; i < l; i++) {
+    // Generate a random index between 0 and characters.length - 1
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  
+  return result;
 }
-
-// Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
+// Do not change the code below.
+
